70414dd784443df7ad3a8f416bc62b07ea10bab8
fcpty2 <focpattahw@gmail.com>
2021-06-30 23:01:13 +0700

    update

---
+++ sentence_sim.py
@@ -0,0 +1,49 @@
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+class BERTModel:
+    def __init__(self):
+        self.tokenizer = self.__get_tokenizer()
+        self.model = self.__get_model()
+
+    def __get_tokenizer(self):
+        return AutoTokenizer.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+
+    def __get_model(self):
+        return AutoModel.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+
+    # initialize dictionary that will contain tokenized sentences
+    def tokenize(self, sentences:list):
+
+        tokens = {'input_ids': [], 'attention_mask': []}
+
+        for sentence in sentences:
+            # tokenize sentence and append to dictionary lists
+            new_tokens = self.tokenizer.encode_plus(sentence, max_length=128, truncation=True,
+                                            padding='max_length', return_tensors='pt')
+
+            tokens['input_ids'].append(new_tokens['input_ids'][0])
+            tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+
+        # reformat list of tensors into single tensor
+        tokens['input_ids'] = torch.stack(tokens['input_ids'])
+        tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+
+        return tokens
+    
+    def model_setup(self, tokens):
+        return self.model(**tokens)
+
+if __name__ == '__main__':
+    sentence_list = [
+            "Three years later, the coffin was still full of Jello.",
+            "The fish dreamed of escaping the fishbowl and into the toilet where he saw his friend go.",
+            "The person box was packed with jelly many dozens of months later.",
+            "Standing on one's head at job interviews forms a lasting impression.",
+            "It took him a month to finish the meal.",
+            "He found a leprechaun in his walnut shell."
+    ]
+
+    bert_obj = BERTModel()
+    tokens = bert_obj.tokenize(sentences=sentence_list)
+    model = bert_obj.model_setup(tokens=tokens)
